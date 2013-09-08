05f061e70e9a93fd72862504dff75912f3e4ea69
Bill Mill <bill.mill@gmail.com>
2013-09-08 13:06:57 -0400

    dl.py works for all sites

--- dl.py
+++ dl.py
@@ -1,15 +1,21 @@
 import re, requests
 
-r = requests.get("http://www.oddschecker.com/football/champions-league/champions-league-group-a/winner",
-                 cookies={"odds_type":"decimal"})
+for group in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+    r = requests.get("http://www.oddschecker.com/football/champions-league/champions-league-group-%s/winner" % group,
+                     cookies={"odds_type":"decimal"})
 
-table = re.search("<table.*?eventTable.*?</table>", r.text, re.DOTALL).group()
+    table = re.search("<table.*?eventTable.*?</table>", r.text, re.DOTALL).group()
 
-sitesrow = re.search("<tr.*?eventTableHeader.*?</tr>", table, re.DOTALL).group()
-sites = re.findall('<a.*?title="(.*?)"', sitesrow)
+    sitesrow = re.search("<tr.*?eventTableHeader.*?</tr>", table, re.DOTALL).group()
+    sites = re.findall('<a.*?title="(.*?)"', sitesrow)
 
-teamrows = re.findall(r'<tr class="eventTableRow.*?</tr>', table, re.DOTALL)
-for row in teamrows:
-    print re.findall("<td.*?>(.*?)<", row)
-#names = [re.findall('_name">(.*?)<', t)[0] for t in teamrows]
-
+    teamrows = re.findall(r'<tr class="eventTableRow.*?</tr>', table, re.DOTALL)
+    for row in teamrows:
+        cols = re.findall("<td.*?>(.*?)<", row)
+        name = cols[1]
+        odds = []
+        for c in cols[3:]:
+            if not c: odds.append(None)
+            else:     odds.append(float(c))
+        assert len(odds) == len(sites)
+        print name, odds
