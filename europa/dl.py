f81af7cf5807bca6a4ae14c6ff71d341bf47bb79
Bill Mill <bill.mill@gmail.com>
2013-09-08 23:52:21 -0400

    a europa viz was super easy
    I don't feel like downloading all those crests though

---
+++ europa/dl.py
@@ -0,0 +1,30 @@
+import re, requests, csv, time
+
+teams = []
+for group in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']:
+    r = requests.get("http://www.oddschecker.com/football/europa-league/europa-league-group-%s/winner" % group,
+                     cookies={"odds_type":"decimal"})
+
+    table = re.search("<table.*?eventTable.*?</table>", r.text, re.DOTALL).group()
+
+    sitesrow = re.search("<tr.*?eventTableHeader.*?</tr>", table, re.DOTALL).group()
+    sites = re.findall('<a.*?title="(.*?)"', sitesrow)
+
+    teamrows = re.findall(r'<tr class="eventTableRow.*?</tr>', table, re.DOTALL)
+    for row in teamrows:
+        cols = re.findall("<td.*?>(.*?)<", row)
+        name = cols[1]
+        odds = []
+        for c in cols[3:]:
+            if not c: odds.append(None)
+            else:     odds.append(float(c))
+        assert len(odds) == len(sites)
+        print name, odds
+        teams.append([name, group] + odds)
+
+t = str(time.time()).split(".")[0]
+with file("raw/odds%s.csv" % t, 'w') as outfile:
+    w = csv.writer(outfile)
+    w.writerow(['name', 'group'] + sites)
+    for row in teams:
+        w.writerow(row)
---
+++ europa/stats.py
@@ -0,0 +1,40 @@
+import glob, csv, re, shutil
+import numpy as np
+
+oddsfile = list(sorted(glob.glob('raw/odds*.csv')))[-1]
+timestamp = re.search('s(.*?)\.', oddsfile).group(1)
+with open(oddsfile) as infile:
+    reader = csv.reader(infile)
+    header = reader.next()
+    teams = [row for row in reader]
+
+fixed = []
+for team in teams:
+    t = team[0:2]
+    for odd in team[2:]:
+        if odd:
+            o = float(odd)
+            # betdaq lists some impossible odds. WTF?
+            if o < 1: o = 1.
+            t.append(o)
+    fixed.append(t)
+
+teams = fixed
+
+summary = []
+for team in teams:
+    odds = team[2:]
+    max_ = max(odds)
+    min_ = min(odds)
+    mean = np.mean(odds)
+    median = np.median(odds)
+    summary.append(team[:2] + [max_, min_, mean, median])
+
+summaryfile = "raw/summary%s.csv" % timestamp
+with file(summaryfile, 'w') as outfile:
+    w = csv.writer(outfile)
+    w.writerow(['name', 'group', 'max', 'min', 'mean', 'median'])
+    for row in summary:
+        w.writerow(row)
+
+shutil.copy2(summaryfile, "summary.csv")
