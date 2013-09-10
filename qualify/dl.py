04c6caace13d14e4415611a3812bba39522dc27c
Bill Mill <bill.mill@gmail.com>
2013-09-09 21:57:55 -0400

    add qualification proability chart

---
+++ qualify/dl.py
@@ -0,0 +1,35 @@
+import re, requests, csv, time
+
+teams = []
+for group in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+    r = requests.get("http://www.oddschecker.com/football/champions-league/champions-league-group-%s/to-qualify" % group,
+                     cookies={"odds_type":"decimal"})
+
+    table = re.search("<table.*?eventTable.*?</table>", r.text, re.DOTALL).group()
+
+    sitesrow = re.search("<tr.*?eventTableHeader.*?</tr>", table, re.DOTALL).group()
+    sites = re.findall('<a.*?title="(.*?)"', sitesrow)
+
+    teamrows = re.findall(r'<tr class="eventTableRow.*?</tr>', table, re.DOTALL)
+    for row in teamrows:
+        cols = re.findall("<td.*?>(.*?)<", row)
+        name = cols[1]
+
+        # normalize PSG name
+        if 'Paris' in name:
+            name = "PSG"
+
+        odds = []
+        for c in cols[3:]:
+            if not c: odds.append(None)
+            else:     odds.append(float(c))
+        assert len(odds) == len(sites)
+        print name, odds
+        teams.append([name, group] + odds)
+
+t = str(time.time()).split(".")[0]
+with file("raw/odds%s.csv" % t, 'w') as outfile:
+    w = csv.writer(outfile)
+    w.writerow(['name', 'group'] + sites)
+    for row in teams:
+        w.writerow(row)
