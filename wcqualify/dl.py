3eab8bdcafd5b6a30489b089bfeaa6008ed0d36b
Bill Mill <bill.mill@gmail.com>
2014-03-02 17:55:43 +0000

    convert dl.py to beautifulsoup

--- wcqualify/dl.py
+++ wcqualify/dl.py
@@ -1,31 +1,38 @@
-import re, requests, csv, time
+import requests, csv, time
+from bs4 import BeautifulSoup
 
 def download_group_odds():
     teams = []
     sites = []
     for group in (chr(ord('a') + x) for x in range(0,8)):
-        url = "http://www.oddschecker.com/football/world-cup-groups/group-{}/world-cup-group-{}/to-qualify".format(group, group)
+        urltemplate = "http://www.oddschecker.com/football/world-cup/group-{}/to-qualify"
+        url = urltemplate.format(group)
         print "getting %s" % url
         r = requests.get(url, cookies={"odds_type":"decimal"})
 
-        table = re.search("<table.*?eventTable.*?</table>", r.text, re.DOTALL).group()
+        soup = BeautifulSoup(r.text)
+        table = soup.find(attrs={"class":"eventTable"})
+        sitesrow = table.find_all("tr", {"class": "eventTableHeader"})
+        sitelinks = sitesrow[0].find_all(lambda t: t.has_attr("title"))
+        sites = [t["title"] for t in sitelinks]
 
-        sitesrow = re.search("<tr.*?eventTableHeader.*?</tr>", table, re.DOTALL).group()
-        # big tables dupe the sites list, so only get uniques
-        sites = list(set(re.findall('<a.*?title="(.*?)"', sitesrow)))
-
-        teamrows = re.findall(r'<tr class="eventTableRow.*?</tr>', table, re.DOTALL)
+        teamrows = table.find_all(attrs={"class": "eventTableRow"})
         for row in teamrows:
-            cols = re.findall("<td.*?>(.*?)<", row)
+            cols = [t.text for t in row.find_all("td")]
             name = cols[1]
 
             if 'any other' in name.lower(): continue
 
             odds = []
-            for c in cols[3:]:
-                if not c or '-' in c: odds.append(None)
-                else:                 odds.append(float(c))
-            assert len(odds) == len(sites)
+            isanodd = lambda t: (t.name=="td" and t.has_attr("class") and
+                                 ('o' in t.attrs["class"] or
+                                  'oi' in t.attrs["class"] or
+                                  'oo' in t.attrs["class"]))
+            rawodds = [t.text for t in row.find_all(isanodd)]
+            for o in rawodds:
+                if not o or '-' in o: odds.append(None)
+                else:                 odds.append(float(o))
+            assert len(odds) == len(sites), "{} {}".format(odds, sites)
             teams.append([name, group] + odds)
 
     return teams, sites
