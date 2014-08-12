602fb71ac76ad921e780490ec806915cd5206d0a
Bill Mill <bill.mill@gmail.com>
2014-08-12 09:05:57 -0400

    handle exceptions better and fix Turkey URL

--- leaguewinners/dl.py
+++ leaguewinners/dl.py
@@ -1,4 +1,10 @@
-import re, requests, csv, time
+import csv
+import re
+import sys
+import time
+import traceback
+
+import requests
 from bs4 import BeautifulSoup
 
 teams = []
@@ -10,35 +16,39 @@ for country, urlpart in [('Germany', 'germany/bundesliga/german-bundesliga'),
                          ('Scotland', 'scottish/premiership'),
                          ('Holland', 'other/netherlands/eredivisie/holland-eredivisie'),
                          ('Portugal', 'other/portugal/primeira-liga/portuguese-super-liga'),
-                         ('Turkey', 'other/turkey/super-lig/turkish-super-league')]:
-    url = "http://www.oddschecker.com/football/%s/winner" % urlpart
-    print "getting %s" % url
-    r = requests.get(url, cookies={"odds_type":"decimal"})
+                         ('Turkey', 'other/turkey/super-lig')]:
+    try:
+        url = "http://www.oddschecker.com/football/%s/winner" % urlpart
+        print "getting %s" % url
+        r = requests.get(url, cookies={"odds_type":"decimal"})
 
-    soup = BeautifulSoup(r.text)
-    table = soup.find(attrs={"class":"eventTable"})
-    sitesrow = table.find_all("tr", {"class": "eventTableHeader"})
-    sitelinks = sitesrow[0].find_all(lambda t: t.has_attr("title"))
-    sites = [t["title"] for t in sitelinks]
+        soup = BeautifulSoup(r.text)
+        table = soup.find(attrs={"class":"eventTable"})
+        sitesrow = table.find_all("tr", {"class": "eventTableHeader"})
+        sitelinks = sitesrow[0].find_all(lambda t: t.has_attr("title"))
+        sites = [t["title"] for t in sitelinks]
 
-    teamrows = table.find_all(attrs={"class": "eventTableRow"})
-    for row in teamrows:
-        cols = [t.text for t in row.find_all("td")]
-        name = cols[1]
+        teamrows = table.find_all(attrs={"class": "eventTableRow"})
+        for row in teamrows:
+            cols = [t.text for t in row.find_all("td")]
+            name = cols[1]
 
-        if 'any other' in name.lower(): continue
+            if 'any other' in name.lower(): continue
 
-        odds = []
-        isanodd = lambda t: (t.name=="td" and t.has_attr("class") and
-                             ('o' in t.attrs["class"] or
-                              'oi' in t.attrs["class"] or
-                              'oo' in t.attrs["class"]))
-        rawodds = [t.text for t in row.find_all(isanodd)]
-        for o in rawodds:
-            if not o or '-' in o: odds.append(None)
-            else:                 odds.append(float(o))
-        assert len(odds) == len(sites), "{} {}".format(odds, sites)
-        teams.append([name, country] + odds)
+            odds = []
+            isanodd = lambda t: (t.name=="td" and t.has_attr("class") and
+                                 ('o' in t.attrs["class"] or
+                                  'oi' in t.attrs["class"] or
+                                  'oo' in t.attrs["class"]))
+            rawodds = [t.text for t in row.find_all(isanodd)]
+            for o in rawodds:
+                if not o or '-' in o: odds.append(None)
+                else:                 odds.append(float(o))
+            assert len(odds) == len(sites), "{} {}".format(odds, sites)
+            teams.append([name, country] + odds)
+    except:
+        print "Unexpected error. skipping"
+        traceback.print_exc()
 
 t = str(time.time()).split(".")[0]
 with file("raw/odds%s.csv" % t, 'w') as outfile:
